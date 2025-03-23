7e70452e403420ee663f2c52856c5c38c94486d9
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-03-23 08:22:44 +0000

    chore(deps): update terraform-module (#201)

--- examples/api_key_auth/main.tf
+++ examples/api_key_auth/main.tf
@@ -23,7 +23,7 @@ module "existing_sm_crn_parser" {
 module "secrets_manager" {
   count                = var.existing_sm_instance_crn == null ? 1 : 0
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "1.25.1"
+  version              = "1.25.4"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   secrets_manager_name = "${var.prefix}-secrets-manager" #tfsec:ignore:general-secrets-no-plaintext-exposure
@@ -36,7 +36,7 @@ module "secrets_manager" {
 # Best practise, use the secrets manager secret group module to create a secret group
 module "secrets_manager_secret_group" {
   source                   = "terraform-ibm-modules/secrets-manager-secret-group/ibm"
-  version                  = "1.2.2"
+  version                  = "1.2.3"
   region                   = local.sm_region
   secrets_manager_guid     = local.sm_guid
   secret_group_name        = "${var.prefix}-certificates-secret-group"   #checkov:skip=CKV_SECRET_6: does not require high entropy string as is static value
@@ -73,7 +73,7 @@ module "public_secret_engine" {
 
 module "secrets_manager_public_certificate" {
   source     = "terraform-ibm-modules/secrets-manager-public-cert/ibm"
-  version    = "1.2.1"
+  version    = "1.2.2"
   depends_on = [module.public_secret_engine]
 
   cert_common_name      = local.cert_common_name
