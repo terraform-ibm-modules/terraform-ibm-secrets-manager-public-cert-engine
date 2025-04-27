2aaa19e9ca5a21863085b396ac763ceea56599ae
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-04-27 03:39:04 +0100

    chore(deps): update terraform terraform-ibm-modules/secrets-manager-secret-group/ibm to v1.3.3 (#219)

--- examples/api_key_auth/main.tf
+++ examples/api_key_auth/main.tf
@@ -36,7 +36,7 @@ module "secrets_manager" {
 # Best practise, use the secrets manager secret group module to create a secret group
 module "secrets_manager_secret_group" {
   source                   = "terraform-ibm-modules/secrets-manager-secret-group/ibm"
-  version                  = "1.3.2"
+  version                  = "1.3.3"
   region                   = local.sm_region
   secrets_manager_guid     = local.sm_guid
   secret_group_name        = "${var.prefix}-certificates-secret-group"   #checkov:skip=CKV_SECRET_6: does not require high entropy string as is static value
--- examples/iam_auth/main.tf
+++ examples/iam_auth/main.tf
@@ -24,7 +24,7 @@ module "secrets_manager" {
 # Best practise, use the secrets manager secret group module to create a secret group
 module "secrets_manager_secret_group" {
   source                   = "terraform-ibm-modules/secrets-manager-secret-group/ibm"
-  version                  = "1.3.2"
+  version                  = "1.3.3"
   region                   = var.region
   secrets_manager_guid     = module.secrets_manager.secrets_manager_guid
   secret_group_name        = "${var.prefix}-certificates-secret-group"   #checkov:skip=CKV_SECRET_6: does not require high entropy string as is static value
