3f054fae5ebec10fa8e0d688c0c59aa7a0f8fbea
Harika <86517031+HarikaPonna@users.noreply.github.com>
2025-11-06 20:04:50 +0530

    feat: added "Next steps" to the UI for DA deployments (#292)

--- solutions/fully-configurable/outputs.tf
+++ solutions/fully-configurable/outputs.tf
@@ -3,3 +3,19 @@
 ##############################################################################
 
 ##############################################################################
+
+# Next steps
+output "next_steps_text" {
+  value       = "Your Public Certificates Engine is ready."
+  description = "Next steps text"
+}
+
+output "next_step_primary_label" {
+  value       = "Go to Secrets Manager Public Certificates Engine"
+  description = "Primary label"
+}
+
+output "next_step_primary_url" {
+  value       = "https://cloud.ibm.com/services/secrets-manager/${var.existing_secrets_manager_crn}?paneId=publicCertificates#/publicCertificates"
+  description = "Primary URL"
+}
