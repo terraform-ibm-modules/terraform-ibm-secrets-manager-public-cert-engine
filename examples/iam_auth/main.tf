6fda7970209af87ba3e2fe7de575498182a938af
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-09-25 23:32:17 +0000

    chore(deps): update dependencies

--- examples/iam_auth/main.tf
+++ examples/iam_auth/main.tf
@@ -13,7 +13,7 @@ module "resource_group" {
 
 module "secrets_manager" {
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "2.15.22"
+  version              = "3.0.0"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   secrets_manager_name = "${var.prefix}-secrets-manager" #tfsec:ignore:general-secrets-no-plaintext-exposure
